1f026b63bba1dee38c32aa19db17eae115641d93
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2024-01-15 15:18:06 -0500

    .

---
+++ Q2_suite_reel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+double limitSequence(double uo);
+
+double errorLimitSequence(double limitNum, int Numsequence);
+
+int main()
+{
+
+    cout << limitSequence(3);
+
+
+
+
+    return 0;
+}
+double limitSequence(double uo){
+
+   
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "Q2_suite_reel.cpp"
+
+
+
+
+
+int main()
+{
+
+
+    cout << "ok";
+
+
+
+
+}
